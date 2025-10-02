3382ccc556572038c0c2e3a57348f6371a7bea02
very-happy-coder <dcissohappy@gmail.com>
2025-10-02 15:13:30 -0400

    yahooooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewTeleop.java
@@ -7,23 +7,25 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="shooter_test",group="")
+@TeleOp(name="GOOSETELEOP",group="")
 public class NewTeleop extends LinearOpMode {
     public boolean flywheelRunning = false;
+    public int direction = 1;
     @Override
     public void runOpMode() {
         // INIT PRESSED: declare & set up everything
         Robot.set(hardwareMap,this);
 
+        /*
         Servo light1 = Servos.create("leftlight");
         Servo light2 = Servos.create("rightlight");
         light1.setPosition(0.7);
         light2.setPosition(0.7);
-
+*/
         DcMotorEx flywheel1 = Motors.create("bl",true);
         DcMotorEx flywheel2 = Motors.create("br",true);
-        Motors.changeRangeOfMotion(flywheel2,false);
-        changeFlywheel(flywheel1,flywheel2,flywheelRunning);
+        Motors.changeRangeOfMotion(flywheel1,false);
+        changeFlywheel(flywheel1,flywheel2,direction,flywheelRunning);
 
         /*
         Wheels.create(
@@ -42,15 +44,17 @@ public class NewTeleop extends LinearOpMode {
             // configure controls
             //Wheels.TeleOpDrive(1);
 
-            if (gamepad1.circleWasPressed()) { flywheelRunning = changeFlywheel(flywheel1,flywheel2,flywheelRunning); }
+            if (gamepad1.circleWasPressed()) { flywheelRunning = changeFlywheel(flywheel1,flywheel2,direction,flywheelRunning); }
+            //if (gamepad1.leftBumperWasPressed()) { direction *= -1; }
 
+            telemetry.addData("Direction?",direction);
             telemetry.addData("Flywheels running?",flywheelRunning);
             telemetry.update();
         }
     }
-    public static boolean changeFlywheel(DcMotorEx flywheel1, DcMotorEx flywheel2, boolean flywheelRunning) {
-        Motors.runControlled(flywheel1,(flywheelRunning) ? 1 : 0);
-        Motors.runControlled(flywheel2,(flywheelRunning) ? 1 : 0);
+    public static boolean changeFlywheel(DcMotorEx flywheel1, DcMotorEx flywheel2, int direction, boolean flywheelRunning) {
+        Motors.runControlled(flywheel1,(flywheelRunning) ? direction : 0);
+        Motors.runControlled(flywheel2,(flywheelRunning) ? direction : 0);
         return !flywheelRunning;
     }
 }
