3c2d547d415c2357f6ab4527a2118dbbc53c6c33
Adhithya Yuvaraj <adhyuva8@gmail.com>
2025-10-17 19:32:56 -0400

    ADHITHYA GOOSE CODE YAAAAYY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GooseGOTHAM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GooseGOTHAM.java
@@ -6,6 +6,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 @TeleOp(name = "Goose Decode v1.0")
 public class GooseGOTHAM extends LinearOpMode {
@@ -13,11 +16,13 @@ public class GooseGOTHAM extends LinearOpMode {
     //This was made by Adhithya Yuvaraj XD
 
     //lets instanitate the 7 motors!
-    DcMotor motorleft, motorright, frontright, frontleft, backright, backleft, intake;
-
+    DcMotor motorright, motorleft, frontright, frontleft, backright, backleft, intake;
 
     //servo kick motor
     Servo kickBall;
+
+    double motorPower = 0;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -33,12 +38,12 @@ public class GooseGOTHAM extends LinearOpMode {
 
         kickBall = hardwareMap.get(Servo.class, "kickBall");
 
-
+        //EX
 
         //UNO REVERSE THOSE MOTORS
         frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
         backleft.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorright.setDirection(DcMotor.Direction.REVERSE);
+        motorleft.setDirection(DcMotor.Direction.REVERSE);
         intake.setDirection(DcMotor.Direction.REVERSE);
 
         //INIT PART DONE!
@@ -47,48 +52,55 @@ public class GooseGOTHAM extends LinearOpMode {
 
         //THIS RUNS ON START!
         while(opModeIsActive()){
-
             //Circle, Cross, Triangle, Square Stuff
-            if(gamepad1.circleWasPressed()) { //MAX SPEED DONT CLICK HIGH SPEED GO BRRRRRR
-                motorleft.setPower(1);
-                motorright.setPower(1);
-                telemetry.addData("Motor Speed Left: ",motorleft.getPower());
-                telemetry.addData("Motor Speed Right: ",motorright.getPower());
-            }else if (gamepad1.crossWasPressed()){ //decrease speed
-                motorleft.setPower(motorleft.getPower()-0.05);
-                motorright.setPower(motorright.getPower()-0.05);
+            if(gamepad2.circleWasPressed()) { //MAX SPEED DONT CLICK HIGH SPEED GO BRRRRRR
+                motorleft.setPower(0.3);
+                motorright.setPower(0.3);
+                motorPower = 0.3;
                 telemetry.addData("Motor Speed Left",motorleft.getPower());
                 telemetry.addData("Motor Speed Right: ",motorright.getPower());
-            }else if (gamepad1.triangleWasPressed()){ //increase speed
-                motorleft.setPower(motorleft.getPower()+0.05);
-                motorright.setPower(motorright.getPower()+0.05);
+            }else if (gamepad2.triangleWasPressed()){ //decrease speed
+                motorleft.setPower(motorPower);
+                motorright.setPower(motorPower);
                 telemetry.addData("Motor Speed Left",motorleft.getPower());
                 telemetry.addData("Motor Speed Right: ",motorright.getPower());
-            }else if(gamepad1.squareWasPressed()){ //Zero Speed
+            }else if (gamepad2.crossWasPressed()){ //increase speed
                 motorleft.setPower(0);
                 motorright.setPower(0);
                 telemetry.addData("Motor Speed Left",motorleft.getPower());
                 telemetry.addData("Motor Speed Right: ",motorright.getPower());
+            }else if(gamepad2.squareWasPressed()){ //Zero Speed
+                motorleft.setPower(0.6);
+                motorright.setPower(0.6);
+                motorPower = 0.6;
+                telemetry.addData("Motor Speed Left",motorleft.getPower());
+                telemetry.addData("Motor Speed Right: ",motorright.getPower());
+
             }
 
-            //intaking
-            if(gamepad1.leftBumperWasPressed()){
+            //decreasing and increasing
+            if(gamepad2.leftBumperWasPressed()){
+                motorleft.setPower(motorleft.getPower() - 0.05);
+                motorright.setPower(motorleft.getPower() - 0.05);
+                motorPower = motorright.getPower();
+            }else if(gamepad2.rightBumperWasPressed()){
+                motorleft.setPower(motorleft.getPower() + 0.05);
+                motorright.setPower(motorleft.getPower() + 0.05);
+                motorPower = motorright.getPower();
+            }
+
+            //SHOOT!
+
+
+            //intake dpad
+            if(gamepad2.dpad_up){
                 intake.setPower(1);
-            }else if(gamepad1.rightBumperWasPressed()){
+            }else if(gamepad2.dpad_down){
                 intake.setPower(0);
-            }
-
-            //kick ball dpad
-            if(gamepad1.dpad_up){
-                //RESET
+            }else if(gamepad2.dpad_right){
                 kickBall.setPosition(1);
-            }else if(gamepad1.dpad_left){
-                //SHOOT
+            }else if(gamepad2.dpad_left){
                 kickBall.setPosition(0);
-            }
-            else if(gamepad1.dpad_right){
-                //GET READY
-                kickBall.setPosition(0.5);
             }
 
             //WHEEEEEEEELLLSSSSSSS GO WEEEEEEE
@@ -102,10 +114,10 @@ public class GooseGOTHAM extends LinearOpMode {
             frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            frontleft.setPower(y+x+r);
-            frontright.setPower(y-x-r);
-            backleft.setPower(y-x+r);
-            backright.setPower(y+x-r);
+            frontleft.setPower((y+x+r)*.3);
+            frontright.setPower((y-x-r)*.3);
+            backleft.setPower((y-x+r)*.3);
+            backright.setPower((y+x-r)*.3);
 
             //update update update
             telemetry.update();
